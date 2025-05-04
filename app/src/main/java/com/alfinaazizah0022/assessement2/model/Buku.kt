deaddb062ba23e75987f7e645fb24290359b3792
Alfina Azizah <alfinaazizah1@gmail.com>
2025-05-04 14:49:07 +0700

    Menyiapkan data di ViewModel

---
+++ app/src/main/java/com/alfinaazizah0022/assessement2/model/Buku.kt
@@ -0,0 +1,9 @@
+package com.alfinaazizah0022.assessement2.model
+
+data class Buku(
+    val id: Long,
+    val judul: String,
+    val penulis: String,
+    val isbn: String,
+    val kategori: String
+)
